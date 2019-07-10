91173c815a834e7e3d866e168e5d120bdfda8800
Eduardo Hernandorena <eduuardobh@gmail.com>
2019-07-10 11:08:16 -0300

    Padronizado o Auto-Increment da classe.

--- src/main/java/br/org/fiergs/scholarityservice/entities/Scholarity.java
+++ src/main/java/br/org/fiergs/scholarityservice/entities/Scholarity.java
@@ -2,19 +2,21 @@ package br.org.fiergs.scholarityservice.entities;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = false)
 @Entity
+@Table(name = "CAD_SCHOLARITY")
+@SequenceGenerator(name = "seqCad_Scholarity", sequenceName = "SEQCAD_SCHOLARITY", allocationSize = 1)
 public class Scholarity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seqCad_Scholarity")
     private Long id;
 
     private boolean status;
